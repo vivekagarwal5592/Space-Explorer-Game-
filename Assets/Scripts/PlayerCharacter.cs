1b33c133d93072a279610b87d11a71fd7c23e8a7
vivek agarwal <vivekagarwal5592@gmail.com>
2017-09-17 19:20:14 -0700

    added game over feature

--- Assets/Scripts/PlayerCharacter.cs
+++ Assets/Scripts/PlayerCharacter.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 public class PlayerCharacter : MonoBehaviour {
 	private float _health;
 	public Text health;
+	public Text game_over;
 
 	void Start() {
 
@@ -16,11 +17,19 @@ public class PlayerCharacter : MonoBehaviour {
 		_health = 100.0f;
 		health.text = "Health: " + _health.ToString();
 
+
 	}
 
 	public void Hurt(float damage) {
 		_health -= damage;
 		//Debug.Log("Health: " + _health);
+
+		if(_health <=0){
+			game_over.text = "Game Over";
+			health.text = "Health: " + 0;
+			Time.timeScale = 0; 
+		}
+
 		health.text = "Health: " + _health.ToString();
 	}
 
